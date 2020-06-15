af27ff1ee713c9ba567c0d153c46fed579e4c049
myungjin-k <63644217+myungjin-k@users.noreply.github.com>
2020-06-15 18:23:39 +0900

    initaial commit

---
+++ src/test/java/me/myungjin/social/SocialApplicationTests.java
@@ -0,0 +1,13 @@
+package me.myungjin.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
