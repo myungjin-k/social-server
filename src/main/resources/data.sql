d56dd4185de233748091620bb95b3a1a6ea5a10c
myungjin-k <63644217+myungjin-k@users.noreply.github.com>
2020-06-30 19:51:12 +0900

    데이터 추가   -- 포스트 기능 컨트롤러 테스트 완료

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,8 +7,12 @@ INSERT INTO posts(seq,user_seq,contents,like_count,comment_count,create_at) VALU
 INSERT INTO posts(seq,user_seq,contents,like_count,comment_count,create_at) VALUES (null,1,'test01 second post',0,0,'2019-03-12 09:45:00');
 INSERT INTO posts(seq,user_seq,contents,like_count,comment_count,create_at) VALUES (null,1,'test01 third post',0,0,'2019-03-20 19:05:00');
 INSERT INTO posts(seq,user_seq,contents,like_count,comment_count,create_at) VALUES (null,2,'test02 post',0,0,'2019-03-20 15:13:20');
+INSERT INTO posts(seq,user_seq,contents,like_count,comment_count,create_at) VALUES (null,4,'mjkim first post',1,1,'2019-03-01 13:10:00');
+INSERT INTO posts(seq,user_seq,contents,like_count,comment_count,create_at) VALUES (null,4,'mjkim second post',0,0,'2019-03-12 09:45:00');
+INSERT INTO posts(seq,user_seq,contents,like_count,comment_count,create_at) VALUES (null,4,'mjkim third post',0,0,'2019-03-20 19:05:00');
 
 INSERT INTO comments(seq,user_seq,post_seq,contents,create_at) VALUES (null,1,1,'first comment','2019-03-01 13:15:00');
 INSERT INTO comments(seq,user_seq,post_seq,contents,create_at) VALUES (null,2,4,'first comment','2019-03-01 13:15:00');
 
 INSERT INTO connections(seq,user_seq,target_seq,granted_at,create_at) VALUES (null,1,2,'2019-03-31 13:00:00','2019-03-31 00:10:00');
+INSERT INTO connections(seq,user_seq,target_seq,granted_at,create_at) VALUES (null,4,1,'2019-03-31 13:00:00','2019-03-31 00:10:00');
