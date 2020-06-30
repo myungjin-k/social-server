d65afbbc34d869df9c964dab0e857cccc3236fe5
myungjin-k <63644217+myungjin-k@users.noreply.github.com>
2020-06-30 19:50:40 +0900

    PageableHandlerArgumentResolver Bean 생성 누락으로 수정

--- src/main/java/me/myungjin/social/configure/WebMvcConfigure.java
+++ src/main/java/me/myungjin/social/configure/WebMvcConfigure.java
@@ -1,15 +1,33 @@
 package me.myungjin.social.configure;
 
+import me.myungjin.social.configure.support.SimpleOffsetPageRequest;
+import me.myungjin.social.configure.support.SimpleOffsetPageableHandlerMethodArgumentResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
+import java.util.List;
+
 @Configuration
 public class WebMvcConfigure implements WebMvcConfigurer {
 
-  private final String baseApiPath = "api";
+    @Bean
+    public SimpleOffsetPageableHandlerMethodArgumentResolver simpleOffsetPageableHandlerMethodArgumentResolver() {
+        SimpleOffsetPageableHandlerMethodArgumentResolver resolver = new SimpleOffsetPageableHandlerMethodArgumentResolver();
+        resolver.setFallbackPageable(new SimpleOffsetPageRequest(0, 5));
+        return resolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(simpleOffsetPageableHandlerMethodArgumentResolver());
+    }
+
+    private final String baseApiPath = "api";
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
