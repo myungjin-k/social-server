286b1a93e8b97aa51ddc93ce47f31034de8eea98
myungjin-k <63644217+myungjin-k@users.noreply.github.com>
2020-07-01 20:24:46 +0900

    AWS s3 Test

--- src/main/java/me/myungjin/social/configure/ServiceConfigure.java
+++ src/main/java/me/myungjin/social/configure/ServiceConfigure.java
@@ -1,5 +1,10 @@
 package me.myungjin.social.configure;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.zaxxer.hikari.HikariDataSource;
 import me.myungjin.social.security.Jwt;
 import me.myungjin.social.util.MessageUtils;
@@ -42,5 +47,19 @@ public class ServiceConfigure {
     MessageSourceAccessor messageSourceAccessor = new MessageSourceAccessor(messageSource);
     MessageUtils.setMessageSourceAccessor(messageSourceAccessor);
     return messageSourceAccessor;
+  }
+
+  @Bean
+  public AmazonS3 amazonS3Client(AwsConfigure awsConfigure) {
+    return AmazonS3ClientBuilder.standard()
+            .withRegion(Regions.fromName(awsConfigure.getRegion()))
+            .withCredentials(
+                    new AWSStaticCredentialsProvider(
+                            new BasicAWSCredentials(
+                                    awsConfigure.getAccessKey(),
+                                    awsConfigure.getSecretKey())
+                    )
+            )
+            .build();
   }
 }
